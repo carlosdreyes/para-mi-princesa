a9d2a412be3f0ccac67b471ab1d483c6b5ced7f9
carlosdreyes <carlosdreyes1601@gmail.com>
2025-10-24 20:21:40 -0600

    Add files via upload

---
+++ carta1.js
@@ -0,0 +1,6 @@
+    function carta1(){
+let resultadoDiv = document.getElementById("r2");
+  resultadoDiv.textContent =" Te amo tanto que siento que explotare, me encantas en todos los sentisoa, amo absolutamente todo en ti, eres lo que siempre pedi y suplique, eres el amor en persona, eres la luz de mi vida. De verdad nunca imagine amar tanto a alguien y querer un futuro juntos, pero ahora lo hago, no me imagino otro fututo que no sea estando junnto a ti te necesito y necesito tu amor tanto como necesito respirar, eres mi motivo de existir y avanzar para poder darte un buen futuro";
+
+
+}
